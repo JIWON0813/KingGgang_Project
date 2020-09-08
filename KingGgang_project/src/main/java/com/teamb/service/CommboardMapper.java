7513407b5ec287fd516efb59fef5896205cebf54
LG <LG@DESKTOP-UNEHFFI>
2020-09-08 18:36:54 +0900

    commit 20200908 18:36

--- KingGgang_project/src/main/java/com/teamb/service/CommboardMapper.java
+++ KingGgang_project/src/main/java/com/teamb/service/CommboardMapper.java
@@ -34,10 +34,10 @@ public class CommboardMapper {
 		  return res;
 	}
 	
-	public int deleteAllBoard(int comm_memberNum,int boardNum) {   
-		  int res = sqlSession.insert("deleteBoard",boardNum);
+	/*public int deleteAllBoard(int comm_memberNum,int boardNum) {   
+		  int res = sqlSession.insert("deleteBoard", boardNum);
 		  return res;
-	}
+	}*/
 
 	public int updateBoard(CommboardDTO dto) {
 		int res = sqlSession.update("updateBoard", dto);
